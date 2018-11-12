9f983e141a4686c6139147a26c58f8d4f8c8368c
godske2 <22340441+godske2@users.noreply.github.com>
2018-11-12 14:09:14 +0100

    Update mainFile.m

--- DroneDetection/mainFile.m
+++ DroneDetection/mainFile.m
@@ -1,4 +1,33 @@
 % TEST_FILE Test the direction command
 clear;
 
-angleCRP = direction('sample_data/log.all_csi.6.7.6',1)
+start='C:\Users\lukas\Desktop\matlab_tester\read_from_here';
+folder = dir(start);
+[large,z]=size(folder);
+large=large-2;
+id=0;
+
+while 1
+    for c = 1:large
+        if(isempty(strfind(folder(2+c).name, 'FF-D9-D3'))==0)
+            id=c;
+            break;
+        end
+    end
+    if(id>0) 
+        break;
+    end
+end
+check = dir(strcat(start,'\',folder(2+id).name));
+[numb,z] = size(check);
+numb=numb-2
+path=strcat(start,'\',folder(2+id).name)
+
+if(numb>0)
+    %her køres funktionen med path
+    %angleCRP = direction(insert_path_to_file_csi,1)
+end
+
+
+
+
