a093d3352371ccb6b50dfa4df4836b6d07f52a00
Andreas Fink <finkz96@gmail.com>
2018-11-19 10:49:55 +0100

    Direction-calculatoin works with many antennas

--- DroneDetection/direction.m
+++ DroneDetection/direction.m
@@ -1,6 +1,9 @@
 %DIRECTION Determines direction from given MEX-file
-function ret = direction(mexfile, packet)
-    d_antenna = 0.06;   % distance between antennas (|M1-M2|)
+function thetaCRP = direction(mexfile, packet)
+    d_antenna = 0.06;   % Distance between antennas (|M1-M2|)
+    f = 2.4E9;          % Signal frequency
+    l_f = 299792458/f;  % Wavelength of signal (c/f)
+    thetaCRP = 0;       % Return value
 
     %Add subfolder containing provided MATLAB-scripts from CSI-tool
     folder = fileparts(which(mfilename)); 
@@ -16,15 +19,23 @@ function ret = direction(mexfile, packet)
     phaseA = unwrap(angle(squeeze(csi(:,1,:)).')); 
     phaseB = unwrap(angle(squeeze(csi(:,2,:)).'));
     phaseC = unwrap(angle(squeeze(csi(:,3,:)).'));
-    phaseAB = mean(phaseB - phaseA);
-    phaseAC = mean(phaseC - phaseA);
-    phaseBC = mean(phaseC - phaseB);
-
-    % Angle calculation
-    theta1 = asin((sign(phaseAB)*0.125/2*((pi-abs(phaseAB))/pi))/d_antenna);
-    theta2 = asin((sign(phaseAC)*0.125/2*((pi-abs(phaseAC))/pi))/d_antenna);
-    theta3 = asin((sign(phaseBC)*0.125/2*((pi-abs(phaseBC))/pi))/d_antenna);
-    thetaCRP = (theta1+(theta2-pi/3)+(theta3+pi/3))/3;
     
-    ret = thetaCRP;
+    %Determine size of array. phaseA is a 30xn array, where n
+    %corresponds to the amount of transmitter-antennas.
+    [~,n] = size(phaseA);
+    
+    %Determine angle for each transmitter-signal
+    for i = 1:n
+       phaseAB = mean(phaseB(:,i) - phaseA(:,i));
+       phaseAC = mean(phaseC(:,i) - phaseA(:,i));
+       phaseBC = mean(phaseC(:,i) - phaseB(:,i));
+       
+       theta1 = asin((sign(phaseAB)*l_f/2*((pi-abs(phaseAB))/pi))/d_antenna);
+       theta2 = asin((sign(phaseAC)*l_f/2*((pi-abs(phaseAC))/pi))/d_antenna);
+       theta3 = asin((sign(phaseBC)*l_f/2*((pi-abs(phaseBC))/pi))/d_antenna);
+       thetaCRP = thetaCRP + (theta1+(theta2-pi/3)+(theta3+pi/3))/3;
+    end
+    
+    % Take the average value of thetaCRP-calculations
+    thetaCRP = thetaCRP/n;
 end
