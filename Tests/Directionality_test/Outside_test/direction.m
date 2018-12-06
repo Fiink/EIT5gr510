c1a20dc07860bcac5e2b75cc7a6df52ced30b68c
Andreas Fink <finkz96@gmail.com>
2018-12-06 11:27:41 +0100

    test fix

--- Tests/Directionality_test/Outside_test/direction.m
+++ Tests/Directionality_test/Outside_test/direction.m
@@ -1,4 +1,4 @@
-file = 'CSI-filer/0nr1.dat';
+file = 'CSI-filer/60nr5.dat';
 
 
 dPhaseAvg = 0;
@@ -22,44 +22,42 @@ X = ['Vaelg pakke - max ',num2str(temp),':  '];
 packet = input(X);
 
 csi = get_scaled_csi(csi_trace{packet});
-[antalrx,~,~] = size(csi);  % Tjek om det er en afsendt pakke i stedet
-if antalrx == 1
-    disp('NEJ FUCK DIG!');
-    return
-end
 
 
 %Compute phase for antennas
 phaseA = unwrap(angle(squeeze(csi(:,1,:)).')); 
 phaseB = unwrap(angle(squeeze(csi(:,2,:)).'));
-phaseC = unwrap(angle(squeeze(csi(:,3,:)).'));
+%phaseC = unwrap(angle(squeeze(csi(:,3,:)).'));
 
 %Determine size of arrays. phaseA is a 30xn array, where n
 %corresponds to the amount of transmitter-antennas.
 [~,TXAntennas] = size(phaseA);
+if TXAntennas == 30
+    TXAntennas = 1;
+end
 
 %Determine which antenna is closest to signal source from permutation
 %1 = antenna A, 2 = antenna B, 3 = antenna C
-sourceAntenna = csi_trace{packet,1}.perm(1);
-X = ['Indikatorantenne: ',num2str(sourceAntenna)];
-disp(X);
-disp(' ');
+% sourceAntenna = csi_trace{packet,1}.perm(1);
+% X = ['Indikatorantenne: ',num2str(sourceAntenna)];
+% disp(X);
+% disp(' ');
 %Determine angle for each transmitter-signal
 for i = 1:TXAntennas
         X = ['-----For loop iteration ',num2str(i),'-----'];
         disp(X);
     %Phase difference
-    if sourceAntenna == 3   %Compute angle from antenna pair AB
+%     if sourceAntenna == 3   %Compute angle from antenna pair AB
         dPhase = phaseB(:,i) - phaseA(:,i);
-    elseif sourceAntenna == 2 %Compute angle from antenna pair AC
-        dPhase = phaseC(:,i) - phaseA(:,i);
-    elseif sourceAntenna == 1 %Compute angle from antenna pair BC
-        dPhase = phaseC(:,i) - phaseB(:,i);
-    else
-        disp('Error: invalid perm');
-        ret = -1; %Indicates an error (-1 is not a valid angle)
-        return
-    end
+%     elseif sourceAntenna == 2 %Compute angle from antenna pair AC
+%         dPhase = phaseC(:,i) - phaseA(:,i);
+%     elseif sourceAntenna == 1 %Compute angle from antenna pair BC
+%         dPhase = phaseC(:,i) - phaseB(:,i);
+%     else
+%         disp('Error: invalid perm');
+%         ret = -1; %Indicates an error (-1 is not a valid angle)
+%         return
+%     end
 
     %Change phase direction if phase difference is more than pi
 %         if mean(dPhase) > pi  
@@ -123,13 +121,13 @@ else
     return
 end
 
-if sourceAntenna == 3       % Antenna C closest
-    ret = mod(ret, 360);        % Take mod_360, as ret may be -90 to 0
-elseif sourceAntenna == 2   % Antenna B closest
-    ret = ret + 120;            % Shift by 120 degrees
-elseif sourceAntenna == 1   % Antenna A closest
-    ret = ret + 240;            % Shift by 240 degrees
-end
+% if sourceAntenna == 3       % Antenna C closest
+%     ret = mod(ret, 360);        % Take mod_360, as ret may be -90 to 0
+% elseif sourceAntenna == 2   % Antenna B closest
+%     ret = ret + 120;            % Shift by 120 degrees
+% elseif sourceAntenna == 1   % Antenna A closest
+%     ret = ret + 240;            % Shift by 240 degrees
+% end
 
 disp(' ');
 X = ['Endelig faseforskeL: ',num2str(dPhaseAvg)];
