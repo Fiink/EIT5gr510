5245e1fc3442c19c95e64a25062bcfada3312315
Andreas Fink <Fiink@users.noreply.github.com>
2018-12-14 15:28:27 +0100

    Final log.py

--- DroneDetection/log.py
+++ DroneDetection/log.py
@@ -2,10 +2,10 @@
 import os,sys,io
 import time,subprocess
 import matlab.engine
-engine=matlab.engine.start_matlab("-Desktop")
 
-engine.addpath(r'/home/user/Documents/matlab',nargout=0)
 print('Engine started...');
+engine=matlab.engine.start_matlab("-Desktop")
+engine.addpath(r'/home/user/Documents/matlab',nargout=0)
 
 mac =[
     '6c:72:20',
@@ -30,9 +30,12 @@ print("Connecting to Specified WiFi Object")
 result = subprocess.check_output('sudo iw dev wlan0 connect -w accesspoint',shell=True)
 time.sleep(0.2)
 print(result)
-if "failed" in result:
+if "failed" in result:			# No connection established, stop program
+	print("Error: WiFi AP not found")
     os.execv('/log.py',[''])
     exit(1)
+
+print("Forcing high bandwidth")
 time.sleep(0.5)
 os.system("echo 0x1c113 | sudo tee /sys/kernel/debug/ieee80211/phy0/iwlwifi/iwldvm/debug/bcast_tx_rate")
 time.sleep(0.5)
@@ -40,12 +43,13 @@ os.system("echo 0x1c113 | sudo tee /sys/kernel/debug/ieee80211/phy0/iwlwifi/iwld
 time.sleep(0.5)
 os.system("echo 0x1c113 | sudo tee /sys/kernel/debug/ieee80211/phy0/netdev:wlan0/stations/6c:72:20:da:5f:27/rate_scale_table")
 time.sleep(1)
-print("Initiate logging")
+
+print("Initiating logging")
 os.system("sudo linux-80211n-csitool-supplementary/netlink/log_to_file csi.dat")
 print("Tracking Protocol Ended")
 time.sleep(0.5)
 x=0
-if len(result)>10:
+if len(result)>10:	# Check if any data was logged
     kk=result[29:37]
 for x in range(int(len(mac))):
     if mac[x] in kk:
@@ -59,14 +63,10 @@ for x in range(int(len(mac))):
         if x>2:
             print("Go-Pro")
         print(res)
-        print(engine.direction('/csi.dat',1))
-        break
-    res=False
-    print(res)
-
-
-
-
-
-
-
+        angle = engine.direction('/csi.dat',1)
+		if angle < 0:		# Check if an error occured in the MATLAB-script
+			print("Error during MATLAB-script")
+			exit(1)
+        print(angle)
+		break
+    print("Error: MAC-address not recognized")
